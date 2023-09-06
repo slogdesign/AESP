7ecb7c0a7b6b7f9b9ecc73d2e7f3d73dc43da61c
slogdesign <paucamcalderon@gmail.com>
2023-09-05 18:27:49 -0700

    AESP5.5
    geolocation added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Geolocation from './Geolocation'; // Import the Geolocation component
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <section className="main">
-        <h1>ALTERNATIVE EMERGENCY LINES TO POLICE</h1>
-      </section>
-      <div className="grid">
-        <a href="tel:1 (800) 854-7771" className="button1" id="b1">Mental Health Crisis</a>
-        <a href="tel:1 (800) 479-7328" className="button2" id="b2">Domestic Violence & SA</a>
-        <a href="something" className="button3" id="b3">LGBTQ+ Counseling & Legal Help</a>
-        <a href="tel:1 (877) 477-3646" className="button4" id="b4">Elderly Protective Services</a>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <section className="main">
+          <h1>ALTERNATIVE EMERGENCY LINES TO POLICE</h1>
+        </section>
+        <div className="grid">
+          <a href="tel:1 (800) 854-7771" className="button1" id="b1">
+            Mental Health Crisis
+          </a>
+          <a href="tel:1 (800) 479-7328" className="button2" id="b2">
+            Domestic Violence & SA
+          </a>
+          <a href="something" className="button3" id="b3">
+            LGBTQ+ Counseling & Legal Help
+          </a>
+          <a href="tel:1 (877) 477-3646" className="button4" id="b4">
+            Elderly Protective Services
+          </a>
+        </div>
+        <div className="responsive-buttons">
+          <a href="https://dontcallthepolice.com/los-angeles/" className="button5" id="b5">
+            Don't Call The Police
+          </a>
+          <a href="tel:911" className="button6" id="b6">
+            911
+          </a>
+        </div>
+        {/* Include the Geolocation component here */}
+        <Geolocation />
       </div>
-      <div className="responsive-buttons">
-        <a href="https://dontcallthepolice.com/los-angeles/" className="button5" id="b5">Don't Call The Police</a>
-        <a href="tel:911" className="button6" id="b6">911</a>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/Geolocation.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import React, { Component } from 'react';
+class Geolocation extends Component {
+  componentDidMount() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        // Handle the user's location data here
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        // You can use latitude and longitude to perform actions based on geolocation.
+      });
+    } else {
+      // Geolocation is not available in this browser
+      // Handle accordingly, e.g., by showing a message to the user
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {/* Component UI goes here */}
+      </div>
+    );
+  }
+}
+
+export default Geolocation;
