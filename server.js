2109c9e50ec89fa85b11dd7b4a9e9d4e8199dc7a
slogdesign <paucamcalderon@gmail.com>
2023-09-07 14:13:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000; // Define your desired port number
 
 // Middleware and route setup go here
+// Define API routes
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello, World!' });
+  });
+  // Add more API routes here
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
