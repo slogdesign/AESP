b510156380a9aa1fbfc017aa22f254913df93779
slogdesign <paucamcalderon@gmail.com>
2023-09-07 12:13:46 -0700

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,11 @@ const path = require('path');
 module.exports = {
   // ...other webpack configuration options...
 
-  resolve: {
-    fallback: {
-      fs: false,
-      os: require.resolve('os-browserify/browser'),
-      path: require.resolve('path-browserify'),
-      stream: require.resolve('stream-browserify'),
-      crypto: require.resolve('crypto-browserify'),
-      buffer: require.resolve('buffer/'),
-    },
+resolve: {
+  fallback: {
+    'fs': false,
+    'os': require.resolve('os-browserify/browser'),
+    'path': require.resolve('path-browserify'),
+    // Add similar entries for other core modules as needed.
   },
-};
+},
