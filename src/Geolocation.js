430a2a913e80239fd105d80b09233d76b3557841
slogdesign <paucamcalderon@gmail.com>
2023-09-07 14:25:21 -0700

    AESP V7.6

--- src/Geolocation.js
+++ src/Geolocation.js
@@ -3,18 +3,26 @@ import React, { Component } from 'react';
 class Geolocation extends Component {
   componentDidMount() {
     if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        // Handle the user's location data here
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-
-        // You can use latitude and longitude to perform actions based on geolocation.
-      });
+      navigator.geolocation.getCurrentPosition(this.handleLocationSuccess, this.handleLocationError);
     } else {
-      // Geolocation is not available in this browser
-      // Handle accordingly, e.g., by showing a message to the user
+      this.handleLocationError({ message: "Geolocation is not available in this browser" });
     }
   }
+
+  handleLocationSuccess = (position) => {
+    // Handle the user's location data here
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    // You can use latitude and longitude to perform actions based on geolocation.
+    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+  };
+
+  handleLocationError = (error) => {
+    // Handle geolocation error
+    console.error(`Geolocation error: ${error.message}`);
+    // You can provide a user-friendly message or take other actions as needed.
+  };
 
   render() {
     return (
