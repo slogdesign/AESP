8344bbb8126f0090d928d7d2bc39b64a1771b5fc
slogdesign <paucamcalderon@gmail.com>
2023-09-07 12:16:45 -0700

    Commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path');
 
 module.exports = {
   // ...other webpack configuration options...
+  target: 'node', // Specify the build target as Node.js
 
-resolve: {
-  fallback: {
-    'fs': false,
-    'os': require.resolve('os-browserify/browser'),
-    'path': require.resolve('path-browserify'),
-    // Add similar entries for other core modules as needed.
+  resolve: {
+    fallback: {
+      'fs': false,
+      'os': require.resolve('os-browserify/browser'),
+      'path': require.resolve('path-browserify'),
+      // Add similar entries for other core modules as needed.
+    },
   },
-},
 };
